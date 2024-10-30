05a119604e38e244c88cb4e8bc491c29c184a865
Pravin10000 <pravint@graspear.com>
2024-10-30 17:15:36 +0530

    generate UUID based on the input value

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [uuid, setUuid] = useState("");
+  const [uuids, setUuids] = useState([]);
+  const [length, setLength] = useState(30);
 
-  // Generate UUID on button click
-  const generateUuid = () => {
-    const newUuid = uuidv4();
-    setUuid(newUuid);
+  // Generate 30 UUIDs on button click
+  const generateUuids = () => {
+    if (length <= 0) {
+      alert("Enter the count of the UUID");
+
+      return;
+    }
+    const newUuids = Array.from({ length: length }, () => uuidv4());
+    setUuids(newUuids);
   };
 
-  // Copy UUID to clipboard
+  // Copy all UUIDs to clipboard
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(uuid);
-    // alert("UUID copied to clipboard!");
+    const allUuids = uuids.join("\n"); // Join UUIDs with a newline for better readability
+    navigator.clipboard.writeText(allUuids);
+    // alert("UUIDs copied to clipboard!");
   };
 
   return (
@@ -25,20 +32,42 @@ function App() {
         marginTop: "50px",
       }}
     >
-      <button
-        onClick={generateUuid}
-        style={{ padding: "10px 20px", fontSize: "16px" }}
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+        }}
       >
-        Generate UUID
-      </button>
-      {uuid && (
+        <button
+          onClick={generateUuids}
+          style={{ padding: "10px 20px", fontSize: "16px" }}
+        >
+          Generate 30 UUIDs
+        </button>
+        <input
+          type="number"
+          placeholder="Enter the count fo the UUID"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+
+      {uuids.length > 0 && (
         <div style={{ marginTop: "20px", textAlign: "center" }}>
-          <p>{uuid}</p>
+          <ul style={{ listStyleType: "none", padding: 0 }}>
+            {uuids.map((uuid, index) => (
+              <li key={index}>{uuid}</li>
+            ))}
+          </ul>
           <button
             onClick={copyToClipboard}
-            style={{ padding: "10px 20px", fontSize: "16px" }}
+            style={{
+              padding: "10px 20px",
+              fontSize: "16px",
+              marginTop: "10px",
+            }}
           >
-            Copy UUID
+            Copy All UUIDs
           </button>
         </div>
       )}
